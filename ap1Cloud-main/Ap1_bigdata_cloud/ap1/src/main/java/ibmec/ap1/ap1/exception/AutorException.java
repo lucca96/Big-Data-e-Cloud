662fdbbe1e016bb1cd28a5c5075d6a20d4c56549
lucca96 <88782132+lucca96@users.noreply.github.com>
2023-11-23 18:25:52 -0300

    Delete ap1Cloud-main directory

--- ap1Cloud-main/Ap1_bigdata_cloud/ap1/src/main/java/ibmec/ap1/ap1/exception/AutorException.java
+++
@@ -1,9 +0,0 @@
-package ibmec.ap1.ap1.exception;
-
-public class AutorException extends BusinessException {
-    
-    public AutorException(String message) {
-        super(message);
-    }
-    
-}
--- ap1Cloud-main/Ap1_bigdata_cloud/ap1/src/main/java/ibmec/ap1/ap1/exception/LivroException.java
+++
@@ -1,7 +0,0 @@
-package ibmec.ap1.ap1.exception;
-
-public class LivroException extends BusinessException{
-    public LivroException(String message) {
-            super(message);
-        }
-}
